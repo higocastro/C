1797d3415235987942d91e4e4de48366587638df
Higo Castro <higocstr@gmail.com>
2024-10-31 13:39:40 -0300

    Create att.c

---
+++ src/att.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+	2	+
+	3	+
+int main(){
+	4	+
+int ano1;
+	5	+
+printf("Digite o Algum Ano e tecle Enter: \n");
+	6	+
+scanf("%d", &ano1);
+	7	+
+	8	+
+	9	+
+if (ano1 % 4==0){
+	10	+
+printf("O %d é ano bissexto \n", ano1);
+	11	+
+} 
+	12	+
+	13	+
+else if (ano1 + 1+1 ){
+	14	+
+printf("O %d é ano não bissexto \n", ano1);
+	15	+
+	16	+
+}
+	17	+
+	18	+
+return 0;
